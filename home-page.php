a259ebfeebffd2f1e4e9e6d728599cc2d18c8df5
Kimberly Canon <kcano162@mtroyal.ca>
2022-10-24 17:16:23 -0600

    home page created and css design added

---
+++ home-page.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css-files/home-page.css" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+    <div class="header">
+            <h1>Header</h1>
+     </div>
+
+
+    <div class="body">
+
+        <div class="desc">
+            <label>Home Page</label> <br>
+            COMP 3512 - PHP Assignment <br>
+            Izabelle Guevarra, Kimberly Canon
+        </div>
+
+        <div class="column top-genre "> 
+            <ul>
+                <li>Top Genres</li>
+            </ul>
+        </div>
+        <div class="column top-artist"> 
+            <ul>
+                <li>Top Artists</li>
+            </ul>
+        </div>
+        <div class="column popular"> 
+            <ul>
+                <li>Most Popular Songs</li>
+            </ul>
+        </div>
+        <div class="column one-hit"> 
+           <ul>
+            <li>One Hit Wonders</li>
+           </ul>
+        </div>
+        <div class="column acoustic"> 
+            <ul>
+                <li>Longest Acoustic Songs</li>
+            </ul>
+        </div>
+        <div class="column club"> 
+            <ul>
+                <li>At the Club</li>
+            </ul>
+        </div>
+        <div class="column running"> 
+            <ul>
+                <li>Running Songs</li>
+            </ul>
+        </div>
+        <div class="column studying"> 
+            <ul>
+                <li>Studying</li>
+            </ul>
+        </div>
+    </div>
+
+    <Footer>
+        <p>COMP 3512 - PHP Assignment</p> 
+        <p>Izabelle Guevarra, Kimberly Canon</p> 
+    </Footer>
+</body>
+
+</html>
