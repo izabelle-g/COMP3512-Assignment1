a1d70fe05faa3e5cfb969213db2b40e4ff3cd976
izabelle-g <izabelle-g@users.noreply.github.com>
2022-10-25 22:23:56 -0600

    Added main and reset css files
    Both files are from COMP2511 assignments

--- home-page.php
+++ home-page.php
@@ -18,7 +18,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="css-files/home-page.css" rel="stylesheet">
+    <link type="text/css" href="css-files/home-page.css" rel="stylesheet">
+    <link type="text/css" href="css-files/main.css" rel="stylesheet" />
     <title>Home | Song Bank</title>
 </head>
 <body>
